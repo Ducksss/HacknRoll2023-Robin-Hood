8d9e3a238b66f9739bd90c988d5f2e25384b613b
keilokimnida <tkl48leon@gmail.com>
2023-01-14 13:33:01 +0800

    [chore] setup prettier

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React from "react";
+import logo from "@assets/img/logo.svg";
 
 export default function Popup(): JSX.Element {
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
       <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
+        <img
+          src={logo}
+          className="h-36 pointer-events-none animate-spin-slow"
+          alt="logo"
+        />
         <p>
           Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
         </p>
