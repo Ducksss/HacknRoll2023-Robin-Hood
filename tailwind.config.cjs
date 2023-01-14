9943279ff366307b45366863214ac8d2bcfcbf0f
keilokimnida <tkl48leon@gmail.com>
2023-01-14 15:39:12 +0800

    [chore] add tailwind theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      animation: {
-        'spin-slow': 'spin 20s linear infinite',
-      }
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        colors: {
+            "primary-light": "#1fb6ff",
+            "primary-dark": "#7e5bef"
+        },
+        extend: {
+            animation: {
+                "spin-slow": "spin 20s linear infinite"
+            }
+        }
     },
-  },
-  plugins: [],
-}
+    plugins: []
+};
